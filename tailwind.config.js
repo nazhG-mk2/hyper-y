1941f381ed36be729eb4029a16ec5975b96652b7
nazhG <nazhirgonzalez@gmail.com>
2025-02-01 11:53:50 -0500

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -22,7 +23,7 @@ export default {
     },
   },
   // eslint-disable-next-line no-undef
-  plugins: [require('daisyui'),
+  plugins: [daisyui,
   ({ addBase, theme }) => {
     addBase({
       ':root': {
