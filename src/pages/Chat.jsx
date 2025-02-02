224553088a530f5289da840ce1de54e6ee3880fd
nazhG <nazhirgonzalez@gmail.com>
2025-02-02 09:53:02 -0500

    new port

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -11,7 +11,7 @@ import { useTranslation } from 'react-i18next';
 import Error from '../componets/common/Error';
 
 const GROK_URL = 'http://43.202.113.176/v1/chat/completions';
-const ELASTICSEARCH_URL = 'http://18.219.124.9:9999/stream_chat';
+const ELASTICSEARCH_URL = 'http://18.219.124.9:8888/stream_chat';
 
 const grokPrompt = `
 You are an expert on the YMCA globally at all scales of the organization.
