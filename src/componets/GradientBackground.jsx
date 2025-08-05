e80a1dd6a49bb18990ad789707bb2384a82a4b0e
nazhG <nazhirgonzalez@gmail.com>
2025-08-05 10:34:10 -0500

    h

--- src/componets/GradientBackground.jsx
+++ src/componets/GradientBackground.jsx
@@ -2,6 +2,6 @@
 
 export default function GradientBackground() {
   return (
-    <div className="fixed inset-0 -z-10 bg-[length:400%_400%] animate-gradient bg-gradient-to-br from-[#89CFF0] via-[#f6e2ae] to-[#1A77F2]" />
+    <div className="fixed h-[100vh] inset-0 -z-10 bg-[length:400%_400%] animate-gradient bg-gradient-to-br from-[#89CFF0] via-[#f6e2ae] to-[#1A77F2]" />
   );
 }
